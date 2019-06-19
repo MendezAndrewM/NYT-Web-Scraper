b0d42067ae16c5cfee93a8936f5972491a627764
unknown <Mendez.AndrewM@gmail.com>
2019-06-18 21:32:55 -0700

    moved content

--- js/events.js
+++ js/events.js
@@ -1,3 +1,30 @@
+const search = $('')
+const queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?&api-key=qL4fYopCYBypWudl3e9ZCXzoQLVwiflm&q=" + search +
+
+$.ajax({
+    url: queryURL,
+    method: "GET"
+    console.log(response)
+  }).then(function(response) {
+    for (let i = 0; i < results.length; i++) {
+    const results = response.response.docs
+    const listResults = $('#results')
+    const newDiv = $("<div>").addClass('article')
+    const h = $("<h4>")
+    const p = $("<p>")
+
+    h.text(results[i].headline.main);
+    p.text(results[i].byline.original + ' | ' + )
+    
+
+    }
+  })
+
+
+
+
+
+
 let term, recordCount, yearStart, startEnd;
 
 $('.search').click(function (e) { 
@@ -16,3 +43,4 @@ $('.reset').click(function (e) {
     e.preventDefault();
     $('.article').remove();
 });
+
